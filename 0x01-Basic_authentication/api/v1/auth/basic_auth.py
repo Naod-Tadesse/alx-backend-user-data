0863aea58bd6f7cc447f8c2cd39b3155d3cd85c0
Naod Taddese <naodtds@gmail.com>
2024-07-09 10:50:42 +0300

    base64 extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,3 +13,17 @@ from models.user import User
 class BasicAuth(Auth):
     """_summary_
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """base 64 authorization header extraction
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        token = authorization_header.split(' ')[-1]
+        return token
