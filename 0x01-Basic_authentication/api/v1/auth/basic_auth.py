b3d40f97287898550f292eb1923d19eab1219ce8
Naod Taddese <naodtds@gmail.com>
2024-07-09 10:52:40 +0300

    decoder

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -27,3 +27,19 @@ class BasicAuth(Auth):
 
         token = authorization_header.split(' ')[-1]
         return token
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """base64 decoder
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            item_to_decode = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(item_to_decode)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
