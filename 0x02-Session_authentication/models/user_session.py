cfea5818866069c356c36267f5ca09e7ca46f485
Naod Taddese <naodtds@gmail.com>
2024-07-12 06:14:45 +0300

    ad user session model

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""user session model
+"""
+from models.base import Base
+from typing import TypeVar, List, Iterable
+from os import path
+import json
+import uuid
+
+
+class UserSession(Base):
+    """class user session"""
+    def __init__(self, *args: list, **kwargs: dict):
+        """init"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
